3f2d0173c75b929f846cf0a15f248eddc03e02d2
forestrr <afimcy78@gmail.com>
2026-09-11 01:16:03 +0400

    hero

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -5,15 +5,15 @@ export interface Client {
 }
 
 export const clients: Client[] = [
-  { name: "Meydan Hotels & Hospitality", logoSrc: "/images/clients/meydan.png", category: "Hospitality & Leisure" },
-  { name: "Sheraton Hotels & Resorts", logoSrc: "/images/clients/sheraton.png", category: "Hospitality" },
-  { name: "Abu Dhabi Airports", logoSrc: "/images/clients/ad_airports.png", category: "Aviation & Infrastructure" },
-  { name: "Shurooq (Sharjah Investment and Development)", logoSrc: "/images/clients/shurooq.png", category: "Government & Development" },
-  { name: "Dubai World Central (DWC) Airport", logoSrc: "/images/clients/dwc_airport.png", category: "Aviation" },
-  { name: "University of Sharjah", logoSrc: "/images/clients/uni_sharjah.png", category: "Education" },
-  { name: "Hilton Hotels & Resorts", logoSrc: "/images/clients/hilton.png", category: "Hospitality" },
-  { name: "Zayed University", logoSrc: "/images/clients/zayed_uni.png", category: "Education" },
-  { name: "Gargash Enterprises", logoSrc: "/images/clients/gargash.png", category: "Automotive & Commercial" },
+  { name: "Meydan", logoSrc: "/images/clients/meydan.png", category: "Hospitality & Leisure" },
+  { name: "Sheraton", logoSrc: "/images/clients/sheraton.svg", category: "Hotels & Resorts" },
+  { name: "Abu Dhabi Airports", logoSrc: "/images/clients/ad_airports.svg", category: "Aviation & Infrastructure" },
+  { name: "Shurooq", logoSrc: "/images/clients/shurooq.png", category: "Government & Development" },
+  { name: "DWC Airport", logoSrc: "/images/clients/dwc_airport.svg", category: "Dubai Airports" },
+  { name: "University of Sharjah", logoSrc: "/images/clients/uni_sharjah.png", category: "Higher Education" },
+  { name: "Hilton", logoSrc: "/images/clients/hilton.svg", category: "Hotels & Resorts" },
+  { name: "Zayed University", logoSrc: "/images/clients/zayed_uni.svg", category: "Higher Education" },
+  { name: "Gargash Enterprise", logoSrc: "/images/clients/gargash.svg", category: "Corporate & Automotive" },
   { name: "Al Ain Zoo", logoSrc: "/images/clients/al_ain_zoo.png", category: "Public & Recreation" },
-  { name: "Radisson Blu", logoSrc: "/images/clients/radisson_blu.png", category: "Hospitality" },
+  { name: "Radisson Blu", logoSrc: "/images/clients/radisson_blu.svg", category: "Hotels & Resorts" },
 ];
